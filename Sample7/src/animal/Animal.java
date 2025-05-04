8eeeea77dce52a8d9f60574f8b50a039253520c2
akihiro-kato-rera <tainakaritu19910821@yahoo.co.jp>
2025-05-04 17:12:18 +0900

    レッスン７の課題の修正

--- Sample7/src/animal/Animal.java
+++ Sample7/src/animal/Animal.java
@@ -15,20 +15,6 @@ public abstract class Animal {
             this.age = age;
         }
 
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-
-
-        public int getAge() {
-            return age;
-        }
-        public void setAge(int age) {
-            this.age = age;
-        }
 
         public void say() {
             System.out.println(name+"です。"+age+"歳です。");
